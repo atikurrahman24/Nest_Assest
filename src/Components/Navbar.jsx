59cc7d3765cd3fd0e750be2754f989228d6f7a9d
atik <thtatikurrahman24@gmail.com>
2025-12-03 14:00:54 +0600

    Nav Complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsFire } from "react-icons/bs";
+import { HiMenu, HiX } from "react-icons/hi";
 
 export default function Navbar() {
-  return (
-    <div>
+  const [isOpen, setIsOpen] = useState(false);
 
-      <div className='w-11/12 mx-auto'>
-        <div className='flex items-center gap-2 py-4'>
-            <img src="/Images/logo.png" alt="Logo" />
-            <h2 className='text-4xl text-[]'>Nest</h2>
+  return (
+    <div className="w-11/12 mx-auto py-4">
+      <div className="flex items-center justify-between">
+
+        {/* Left side */}
+        <div className="flex items-center gap-12 md:gap-4 lg:gap-16">
+
+          <div className="flex items-center gap-0 md:gap-1 lg:gap-2 cursor-pointer">
+            <img src="/Images/logo.png" alt="Logo" className="w-10 md:w-14 lg:w-16" />
+            <div className="flex flex-col gap-0">
+              <h2 className="text-xl md:text-2xl lg:text-4xl font-semibold text-[#3BB77E]">Nest</h2>
+              <p className="text-[6px] md:text-[7px] lg:text-[8px] text-black/70 -mt-1">MART & GROCERY</p>
+            </div>
+          </div>
+          <p className="flex items-center gap-1 text-[14px] md:text-[16px] lg:text-[20px] cursor-pointer hover:text-green-600"><BsFire className="text-[#3BB77E]" /><span>Hot Deals</span></p>
+          
         </div>
+
+        {/* Mid section */}
+        <div className="flex items-center gap-2 md:gap-4 lg:gap-6 px-2 ">
+
+          {/* Mid menu for md+ screens */}
+          <div className="hidden md:flex overflow-hidden gap-4 md:gap-4 lg:gap-10 ">
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">About</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Shop</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Mega Menu</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Vendors</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Blogs</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Pages</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Contact</p>
+          </div>
+
+          {/* Small screen dropdown */}
+          <div className="relative md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)} className=" -mr-6">{isOpen ? <HiX size={16} /> : <HiMenu size={24} />}</button>
+            {isOpen && (
+              <div className="absolute right-0 mt-2 w-40 bg-white/50 shadow-md rounded-md z-10 flex flex-col">
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">About</p>
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">Shop</p>
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">Mega Menu</p>
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">Vendors</p>
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">Blogs</p>
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">Pages</p>
+                <p className="px-4 py-2 text-sm cursor-pointer hover:bg-green-300">Contact</p>
+              </div>
+            )}
+          </div>
+
+        </div>
+
+        {/* Right side login button */}
+        <button className="bg-[#3BB77E] text-sm md:text-lg lg:text-xl text-white px-2 md:px-3 lg:px-4 py-1 lg:py-1.5 rounded-md">Log In</button>
+
       </div>
     </div>
-  )
+  );
 }
