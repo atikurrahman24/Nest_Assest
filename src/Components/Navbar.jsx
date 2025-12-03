f5464935d7401daf8b88e9dbcae9b0b1398a4aea
atik <thtatikurrahman24@gmail.com>
2025-12-03 17:33:33 +0600

    Banner added, Nav Modified

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,8 +6,9 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="w-11/12 mx-auto py-4">
-      <div className="flex items-center justify-between">
+    <div className=" border-b border-black/10 bg-white shadow-sm sticky top-0 z-50">
+
+      <div className="w-11/12 mx-auto py-2 md:py-3  flex items-center justify-between ">
 
         {/* Left side */}
         <div className="flex items-center gap-12 md:gap-4 lg:gap-16">
@@ -19,7 +20,7 @@ export default function Navbar() {
               <p className="text-[6px] md:text-[7px] lg:text-[8px] text-black/70 -mt-1">MART & GROCERY</p>
             </div>
           </div>
-          <p className="flex items-center gap-1 text-[14px] md:text-[16px] lg:text-[20px] cursor-pointer hover:text-green-600"><BsFire className="text-[#3BB77E]" /><span>Hot Deals</span></p>
+          <p className="flex items-center gap-1 text-[14px] md:text-[16px] lg:text-[20px] cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300"><BsFire className="text-[#3BB77E]" /><span>Hot Deals</span></p>
           
         </div>
 
@@ -27,18 +28,18 @@ export default function Navbar() {
         <div className="flex items-center gap-2 md:gap-4 lg:gap-6 px-2 ">
 
           {/* Mid menu for md+ screens */}
-          <div className="hidden md:flex overflow-hidden gap-4 md:gap-4 lg:gap-10 ">
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">About</p>
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Shop</p>
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Mega Menu</p>
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Vendors</p>
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Blogs</p>
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Pages</p>
-            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600">Contact</p>
+          <div className="hidden md:flex overflow-hidden gap-4 md:gap-4 lg:gap-10 p-2 ">
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">About</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">Shop</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">Mega Menu</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">Vendors</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">Blogs</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">Pages</p>
+            <p className="text-[14px] md:text-[16px] lg:text-[20px]  cursor-pointer hover:text-green-600 hover:scale-110 transition-transform duration-300">Contact</p>
           </div>
 
           {/* Small screen dropdown */}
-          <div className="relative md:hidden">
+          <div className="relative md:hidden pt-2">
             <button onClick={() => setIsOpen(!isOpen)} className=" -mr-6">{isOpen ? <HiX size={16} /> : <HiMenu size={24} />}</button>
             {isOpen && (
               <div className="absolute right-0 mt-2 w-40 bg-white/50 shadow-md rounded-md z-10 flex flex-col">
@@ -56,9 +57,10 @@ export default function Navbar() {
         </div>
 
         {/* Right side login button */}
-        <button className="bg-[#3BB77E] text-sm md:text-lg lg:text-xl text-white px-2 md:px-3 lg:px-4 py-1 lg:py-1.5 rounded-md">Log In</button>
+        <button className="bg-[#3BB77E] text-sm md:text-lg lg:text-xl text-white hover:bg-amber-600 duration-200 px-2 md:px-3 lg:px-4 py-1 lg:py-1.5 rounded-lg">Log In</button>
 
       </div>
+
     </div>
   );
 }
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div>
       <div>
         <Navbar></Navbar>
+        <div className="w-11/12 mx-auto"><img src="/Images/banner.png" alt="Banner" className="w-full"/></div>
+        
       </div>
     </div>
   )
